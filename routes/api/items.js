f1a06e75ee4369831b574797e7a3225aa871b99b
galbeiroc <alcresgu_1989@hotmail.com>
2020-02-19 00:48:55 -0500

    install depedencies, connect to DB, create model and implement route

---
+++ routes/api/items.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+
+//Item Model
+const Item = require('../../models/Items');
+
+//@route GET api/items
+//@desc GET all items
+//@acces public
+router.get('/', (req, res) => {
+  Item.find()
+    .sort({ date: -1 })
+    .then(items => res.json());
+});
+
+//@route POST api/items
+//@desc POST all items
+//@acces public
+router.post('/', (req, res) => {
+  const newItem = new Item({
+    name: req.body.name
+  });
+  newItem.save().then(item => res.json(item));
+});
+
+//@route DELETE api/items/:id
+//@desc DELETE all items
+//@acces public
+router.delete('/:id', (req, res) => {
+  Item.findById(req.param.id)
+    .then(item => item.remove().then(() => res.json({ success: true })))
+    .catch(err => res.status(404).json({ success: false }));
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const items = require('./routes/api/items');
+
+const app = express();
+
+//BodyParser Middleware
+app.use(bodyParser.json());
+
+//Db Config
+const db = require('./config/keys').mongoUri;
+
+//Connect to Mongo
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB COnnected..'))
+  .catch(err => console.error(err));
+
+//Use Routes
+app.use('api/items', items)
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
